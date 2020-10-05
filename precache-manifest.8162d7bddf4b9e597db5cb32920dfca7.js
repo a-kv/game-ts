7bf8f02ca66d89305ca027b80f07f6bfef869166
kvaso <aakvasovaa@gmail.com>
2020-10-05 22:42:58 +0300

    Updates

---
+++ precache-manifest.8162d7bddf4b9e597db5cb32920dfca7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a78a2105ff5890b2fe45b388c09f19c",
+    "url": "/game-ts/index.html"
+  },
+  {
+    "revision": "a62b5e9c89424f38a9fa",
+    "url": "/game-ts/static/css/main.b4d685f6.chunk.css"
+  },
+  {
+    "revision": "e7c10eae22c14c340f74",
+    "url": "/game-ts/static/js/2.db6eec57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-ts/static/js/2.db6eec57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62b5e9c89424f38a9fa",
+    "url": "/game-ts/static/js/main.393418ba.chunk.js"
+  },
+  {
+    "revision": "25c90c8e052f627717e0",
+    "url": "/game-ts/static/js/runtime-main.188334b9.js"
+  },
+  {
+    "revision": "9a80272f15c4763540bf82a1c89b7e62",
+    "url": "/game-ts/static/media/logo.9a80272f.gif"
+  },
+  {
+    "revision": "ebd22e58783e825042e19f19699488d5",
+    "url": "/game-ts/static/media/mic.ebd22e58.png"
+  },
+  {
+    "revision": "6f7f9879571d6d150bbfe5744d1db69b",
+    "url": "/game-ts/static/media/piz.6f7f9879.cur"
+  },
+  {
+    "revision": "fc3397bebceba6fffaf813149ec23aa6",
+    "url": "/game-ts/static/media/prikolnoe-chavkane.fc3397be.mp3"
+  }
+]);
